9175ea560df468905765affcb557311e45d340c4
pau <pllinas@cifpfbmoll.eu>
2021-03-11 16:52:39 +0100

    feat: :zap: Update dependency between classes to apply correctly the DIP Principle

--- dip/src/main/java/edu/pingpong/dip/bbdd/InventarioBBDD.java
+++ dip/src/main/java/edu/pingpong/dip/bbdd/InventarioBBDD.java
@@ -1,10 +1,11 @@
 package edu.pingpong.dip.bbdd;
 
-public class InventarioBBDD {
+import java.util.Map;
 
+public class InventarioBBDD implements Inventario{
 
 	// Metodo de consulta a la base de datos
-	
+	@Override
 	public int numeroProductos(String tienda, String producto) {
 		return BBDD.stocs.get(tienda).get(producto);
 	}
--- dip/src/main/java/edu/pingpong/dip/negocio/ControladorEstoc.java
+++ dip/src/main/java/edu/pingpong/dip/negocio/ControladorEstoc.java
@@ -1,12 +1,12 @@
 package edu.pingpong.dip.negocio;
 
-import edu.pingpong.dip.bbdd.*;
+import edu.pingpong.dip.bbdd.Inventario;
 
-public class ControladorEstoc {
+public class ControladorEstoc implements Controlador{
 
-	private final InventarioBBDD inventario;
+	private final Inventario inventario;
 	
-	public ControladorEstoc(InventarioBBDD inventario) {
+	public ControladorEstoc(Inventario inventario) {
 		this.inventario = inventario;
 	}
 
@@ -17,10 +17,11 @@ public class ControladorEstoc {
 	 * p.e: necesitamos al menos 400 mesas (mesa : 4 letras) y
 	 *    al menos 700 lamparas (lampara: 7 letras)
 	 */
+
+    @Override
 	public boolean necesitaReponer (String tienda, String producto) {
 		int cantidadActual = inventario.numeroProductos(tienda, producto);
 		
 		return  cantidadActual < producto.length() * 100;
 	}
-
 }
